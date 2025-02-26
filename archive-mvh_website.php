cf29607580d1fca9e9961f4ebc384166697e3b99
doxifieq <doxifieq@gmail.com>
2025-02-26 10:30:53 +0100

    [update]

--- archive-mvh_website.php
+++ archive-mvh_website.php
@@ -12,6 +12,23 @@
 
         <div class="wrapper">
             <h1>Websites</h1>
+
+            <div class="stats-grid">
+                <div class="stats-grid-item active-incident">
+                    <p>Active Incidents</p>
+                    <h3>0</h3>
+                </div>
+
+                <div class="stats-grid-item total-incident">
+                    <p>Total Incidents</p>
+                    <h3>0</h3>
+                </div>
+
+                <div class="stats-grid-item latest-incident">
+                    <p>Latest Incident</p>
+                    <h3>None</h3>
+                </div>
+            </div>
         </div>
 
         <?php wp_footer(); ?>
