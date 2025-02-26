9bcfd8ba5bf1d72f01f417a07b2056afa93899b6
doxifieq <doxifieq@gmail.com>
2025-02-26 11:39:06 +0100

    [update]

--- archive-mvh_website.php
+++ archive-mvh_website.php
@@ -66,8 +66,8 @@
                             echo '
                                 <a class="active-websites-item" href="' . get_the_permalink() . '">
                                     <h3>Google</h3>
-                                    <p><span class="dot green"></span>Status Code: 200</p>
                                     <p class="muted">' . $website_url . '</p>
+                                    <p><span class="dot green"></span>Status Code: 200</p>
                                     <p class="muted">0 total incidents</p>
                                 </a>
                             ';
