aafa2f211686c133ae716d84a3bcbc206fea80a2
doxifieq <doxifieq@gmail.com>
2025-02-26 09:55:50 +0100

    [update]

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+function mvh_enqueue_styles() {
+    wp_enqueue_style('mvh-style', '/wp-content/themes/cronjob-test/style.css?dev=' . time());
+}
+
+add_action('wp_enqueue_scripts', 'mvh_enqueue_styles');
+
+add_theme_support('automatic-feed-links');
+add_theme_support('title-tag');
