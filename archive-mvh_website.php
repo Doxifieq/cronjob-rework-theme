b03731e78dad1f0654aaa0eb94767bcdb3c9850b
doxifieq <doxifieq@gmail.com>
2025-02-27 11:40:22 +0100

    [update]

--- archive-mvh_website.php
+++ archive-mvh_website.php
@@ -33,9 +33,8 @@
                 } elseif (str_contains($key, 'status_code_')) {
                     $time = substr($key, 12);
 
-                    if ($time > strtotime('7 day')) $uptime_7d_count++;
-                    
                     if ($time > strtotime('24 hour')) $uptime_24h_count++;
+                    if ($time > strtotime('7 day')) $uptime_7d_count++;
                 }
             }
         }
